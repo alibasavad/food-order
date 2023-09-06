6d03d19b01113d868d03c85a2ee7b720d33606d8
unknown <alibasavad1381@gmail.com>
2023-09-07 00:44:50 +0330

    user register and login

--- src/Routes/routes.js
+++ src/Routes/routes.js
@@ -1,10 +1,21 @@
 import * as controllers from "../Controllers/controllers";
 
 const route = (app) => {
-    app.route('/food')
+  app
+    .route("/food")
     .get(controllers.getFood)
 
-    .post(controllers.addNewFood)
+    .post(controllers.addNewFood);
+
+  app
+    .route("/register")
+
+    .post(controllers.register);
+
+  app
+    .route("/login")
+
+    .post(controllers.login);
 };
 
 export default route;
