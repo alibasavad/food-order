d587527cc23fdd6277b86f6149ffbeb343fe9d76
unknown <alibasavad1381@gmail.com>
2023-09-15 19:36:22 +0330

    version 1.0.0

--- src/Controllers/controllers.js
+++ src/Controllers/controllers.js
@@ -6,13 +6,13 @@ import multer from "multer";
 const Bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
-const Storage = multer.diskStorage({
+const Storage = multer.diskStorage({ // upload directory
   destination: `${__dirname}/../../public/image`,
   filename: (req, file, cb) => {
     cb(null, Date.now() + file.originalname);
   },
 });
 
-export const upload = multer({
+export const upload = multer({ //upload service
   storage: Storage,
 }).single("image");
